7dbd6c943cc6fc5bcb351f42fa6188de9e402883
Mohaned Mohammed <mohandmohamed104@gmail.com>
2025-09-18 09:59:42 +0000

    StoreSeasonRequest.php edited online with Bitbucket

--- app/Http/Requests/StoreSeasonRequest.php
+++ app/Http/Requests/StoreSeasonRequest.php
@@ -8,7 +8,7 @@ class StoreSeasonRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // Adjust if you add policies
+        return true;
     }
 
     public function rules(): array
